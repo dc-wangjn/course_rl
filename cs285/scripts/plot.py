fd8bbf1a8e03ea84d8eef79839a899cbac53c850
dc-wangjn <wangjn.sufedc@outlook.com>
2020-10-12 17:39:30 +0800

    log

---
+++ cs285/scripts/plot.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import os
+
+def dict2array(dict_):
+
+    avg = [di['Eval_AverageReturn'] for di in dict_]
+    std = [di['Eval_StdReturn'] for di in dict_]
+    return avg, std
+
+bc_ant_pth = '../../data/q1_bc_ant_Ant-v2_12-10-2020_16-58-17/return.pkl'
+da_ant_pth = '../../data/q2_dagger_ant_Ant-v2_12-10-2020_16-42-01/return.pkl'
+
+bc_walk_pth = '../../data/q1_bc_walker_Walker2d-v2_12-10-2020_17-00-29/return.pkl'
+da_wals_pth = '../../data/q2_dagger_walker_Walker2d-v2_12-10-2020_17-02-06/return.pkl'
+
+ant_data = []
+walk_data = []
+for pth in [bc_ant_pth,da_ant_pth]:
+    f = open(pth,'rb')
+    data = pickle.load(f)
+    f.close()
+    ant_data.append(data)
+
+for pth in [bc_walk_pth,da_walk_pth]:
+    f = open(pth,'rb')
+    data = pickle.load(f)
+    f.close()
+    walk_data.append(data)
+
+ex_ant = ([di['Expert_AverageReturn'] for di in ant_data[0]],
+          [di['Expert_StdReturn'] for di in ant_data[0]])
+
+bc_ant = dict2array(ant_data[0])
+da_ant = dict2array(ant_data[1])
+
+plt.errorbar(range(bc_ant[0]),bc_ant[0],yerr=bc_ant[1],label='BC')
+plt.errorbar(range(da_ant[0]),da_ant[0],yerr=da_ant[1],label='Dagger')
+plt.errorbar(range(ex_ant[0]),ex_ant[0],yerr=ex_ant[1],label='Expert')
+plt.legend()
+plt.title('Ant Performance')
+plt.savefig('../../data/Ant_performance.png')
+plt.close('all')
+
+ex_walk = ([di['Expert_AverageReturn'] for di in walk_data[0]],
+          [di['Expert_StdReturn'] for di in walk_data[0]])
+
+bc_walk = dict2array(walk_data[0])
+da_walk = dict2array(walk_data[1])
+
+plt.errorbar(range(bc_walk[0]),bc_walk[0],yerr=bc_walk[1],label='BC')
+plt.errorbar(range(da_walk[0]),da_walk[0],yerr=da_walk[1],label='Dagger')
+plt.errorbar(range(ex_walk[0]),ex_walk[0],yerr=ex_walk[1],label='Expert')
+plt.legend()
+plt.title('Walker2d Performance')
+plt.savefig('../../data/Walker2d_performance.png')
